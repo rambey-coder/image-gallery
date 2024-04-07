78cda39f42584c320affe23bc4e37e870f8ec844
Rambey <71680392+rambey-coder@users.noreply.github.com>
2024-04-07 01:48:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,9 +38,8 @@ function App() {
       
       <div className='head-container'>
         <div className='content-container'>
-            <h1>Gallery</h1>
+            <h1>ImageSearch Hub</h1>
             <p>The internet’s source of freely-usable images.</p>
-            <p>Powered by creators everywhere.</p>
             <div className='search-input'>
                 <img src='./Assets/search.svg' alt='search' className='search-icon' />
                 <input type='search' placeholder='Search for photos' onChange={handleChange}/>
